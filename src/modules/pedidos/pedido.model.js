38b1f26d75f9710b56bbdf79806b839c3a8d4224
hleopary <cacholeo@gmail.com>
2026-06-24 17:48:59 -0300

    paso 1, trazabilidad pedidos

--- src/modules/pedidos/pedido.model.js
+++ src/modules/pedidos/pedido.model.js
@@ -1,4 +1,4 @@
-import mongoose from '../../config/db.js';
+ddimport mongoose from '../../config/db.js';
 
 // Schema para los items dentro de un pedido.
 const itemPedidoSchema = new mongoose.Schema({
@@ -22,6 +22,19 @@ const itemPedidoSchema = new mongoose.Schema({
     required: true,
   },
 }, { _id: false }); // No se necesitan IDs para estos subdocumentos.
+
+const historialEstadoSchema = new mongoose.Schema({
+  estado: { type: String, required: true },
+  usuarioId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Usuario',
+    required: true,
+  },
+  fecha: {
+    type: Date,
+    default: Date.now,
+  },
+}, { _id: false });
 
 const pedidoSchema = new mongoose.Schema({
   sucursalId: {
@@ -47,6 +60,19 @@ const pedidoSchema = new mongoose.Schema({
   observaciones: {
     type: String,
     default: '',
+  },
+  precioTotal: {
+    type: Number,
+    required: true,
+    min: [0, 'El precio total no puede ser negativo'],
+  },
+  creadoPor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Usuario',
+    required: true,
+  },
+  historialEstados: {
+    type: [historialEstadoSchema],
   },
 }, {
   // Mongoose manejará createdAt y updatedAt, que mapeamos a nuestros campos existentes.
